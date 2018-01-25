0ad9f4ca7ed809018b10eceefa0c4146d63e8a01
Diego Beghin <diego.beghin@gmail.com>
2018-01-25 18:03:52 +0100

    small fix

--- Rates/config_makeBatchJobs.py
+++ Rates/config_makeBatchJobs.py
@@ -19,13 +19,13 @@ import sys
 '''
 #If you already have a list of input files with the proper format, you may not want to remake it
 #Type True if you want to remake them, False otherwise
-makeInputFilesList = True
+makeInputFilesList = False
 #Directory where your input root files are located
 inputFilesDir = "/eos/cms/store/group/dpg_trigger/comm_trigger/TriggerStudiesGroup/STEAM/xulyu/menu_v4.2/HLTPhysics_PS1p5e34_new"
 
 #Were your input files produced by STEAM? If yes, file_type = "custom"
 #Are these raw data files? If yes, file_type = "RAW"
-file_type = "custom"#"RAW"
+file_type = "RAW"#"custom"
 
 #Directory where the top of your CMSSW release is located
 cmsswDir = "/afs/cern.ch/work/d/dbeghin/Work/2017_STEAM/For_Data/CMSSW_9_2_12/src"
@@ -35,10 +35,10 @@ json_file = "/afs/cern.ch/user/n/ndaci/public/STEAM/JSON/ProcessL1Accept2017/jso
 
 #Do you wish to use any unusual (non-default) options for the batch queue, and the number of files processed per job?
 #If you do, set the following boolean to True
-isUnusual = False
+isUnusual = True
 #If you do, please also specify the following parameters:
 #number of files processed per job
-n = 5
+n = 1
 #Batch queue where you wish to send the jobs
 queue = "8nh"
 '''
@@ -54,7 +54,7 @@ else:
     command = "python batchScriptForRates.py -j %s -e %s -f %s" %(json_file, cmsswDir, file_type)
 
 if isUnusual:
-    command += "-n %s -q %s" %(n, queue)
+    command += " -n %s -q %s" %(n, queue)
 
 os.system(command)
 
