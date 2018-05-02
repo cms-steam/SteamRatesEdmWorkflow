2ed938a96fcba1ec292b6a7a56fd63aa3bd2960a
Diego Beghin <diego.beghin@gmail.com>
2018-05-02 12:21:04 +0200

    improved make_ratesFilesInput to be able to recursively search for root files

--- Rates/make_ratesFilesInput.py
+++ Rates/make_ratesFilesInput.py
@@ -15,10 +15,27 @@ print 'input directory = %s'%opts.inDir
 outfile = open('filesInput.py', 'w')
 outfile.write("fileInputNames = [\n")
 
-ls_command = runCommand("ls " + opts.inDir)
-stdout, stderr = ls_command.communicate()
+keepGoing = True
+nLoop=0
+max_layers=10
+lookhere = []
+lookhere.append(opts.inDir)
+while(keepGoing):
+    lh_buffer = []
+    for i in range(0, len(lookhere)):
+        dirr = lookhere[i]
+        ls_command = runCommand("ls " + dirr)
+        stdout, stderr = ls_command.communicate()
+        for line in stdout.splitlines():
+            if ".root" in line:
+                outfile.write('"' + dirr + '/' + line + '",' + '\n')
+            elif "log" in line:
+                continue
+            else:
+                lh_buffer.append(dirr + '/' + line)
+    lookhere = list(lh_buffer)
+    nLoop += 1
+    if len(lookhere)==0 or nLoop>=max_layers: keepGoing = False
+    if nLoop >= max_layers: print "Too many directory layers (>=%s) before finding any root files, stopping now..." %max_layers
 
-for line in stdout.splitlines():
-    if ".root" in line:
-        outfile.write('"' + opts.inDir + '/' + line + '",' + '\n')
 outfile.write(']')
