188484a7b5cc0f4480476efc032a5d864fe0c4c5
dbeghin <diego.beghin@gmail.com>
2019-07-29 16:37:42 +0200

    MC rates update-2

---
+++ Prod/map_MCdatasets_xs.py
@@ -0,0 +1,7 @@
+#datasets and cross-sections (pb)
+
+datasetCrossSectionMap = {
+'/RelValZMM_13/CMSSW_10_6_0-PUpmx25ns_106X_mcRun2_asymptotic_v3-v1/GEN-SIM-DIGI-RAW-HLTDEBUG' :      2000,
+'/RelValZMM_13/CMSSW_10_4_0_pre4-103X_mc2017_realistic_v2-v1/GEN-SIM-DIGI-RAW' :                     2000,
+'/RelValZMM_13/CMSSW_10_4_0_pre4-PU25ns_103X_mc2017_realistic_v2-v1/GEN-SIM-DIGI-RAW' :              2000,
+}
