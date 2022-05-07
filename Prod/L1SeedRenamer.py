db360f7b8e23207fa60f1b54efa611b0e83d9ff9
Sanu Varghese <74061496+sanuvarghese@users.noreply.github.com>
2022-05-07 16:19:19 +0530

    workaround to undo L1 seed changes
    Useful to run the latest GRun menus on HLTPhysics samples

--- Prod/L1SeedRenamer.py
+++ Prod/L1SeedRenamer.py
@@ -7,11 +7,11 @@ match = {
 'L1_DoubleMu4p5er2p0_SQ_OS_Mass_7to18':'L1_DoubleMu4p5er2p0_SQ_OS_Mass7to18',
 'L1_TripleMu_2_1p5_0OQ_Mass_Max15':'L1_TripleMu_2_1p5_0OQ_Mass_Max_15',
 'L1_TripleMu_2SQ_1p5SQ_0OQ_Mass_Max15':'L1_TripleMu_2SQ_1p5SQ_0OQ_Mass_Max_15',
-'L1_SingleMuShower_Nominal':'L1_MuShower_OneNominal',
+'L1_SingleMuShower_Nominal':'L1_ETMHF100 OR L1_ETMHF110 OR L1_ETM150 OR L1_ETMHF120 OR L1_ETMHF150',
 'L1_SingleMuShower_Tight':'L1_MuShower_OneTight',
 'L1_DoubleMu3_OS_er2p3_Mass_Max14_DoubleEG7p5_er2p1_Mass_Max20':'L1_DoubleMu3_OS_DoubleEG7p5Upsilon',
 'L1_DoubleMu5_OS_er2p3_Mass_8to14_DoubleEG3er2p1_Mass_Max20':'L1_DoubleMu5Upsilon_OS_DoubleEG3',
-'L1_DoubleIsoTau26er2p1_Jet55_RmOvlp_dR0p5':'L1_DoubleIsoTau26er2p1_Jet55_OvRm_dR0p5',
+'L1_DoubleIsoTau26er2p1_Jet55_RmOvlp_dR0p5':'L1_DoubleIsoTau32er2p1',
 'L1_IsoTau52er2p1_QuadJet36er2p5':'L1_QuadJet36er2p5_IsoTau52er2p1',
 'L1_HTT280er_QuadJet_70_55_40_35_er2p5':'L1_HTT280er_QuadJet_70_55_40_35_er2p4',
 'L1_HTT320er_QuadJet_70_55_40_40_er2p5':'L1_HTT320er_QuadJet_70_55_40_40_er2p4',
@@ -32,7 +32,12 @@ match = {
 'L1_DoubleEG10_er1p2_dR_Max0p6':'L1_DoubleEG10er1p22_dR_0p6',
 'L1_DoubleEG10p5_er1p2_dR_Max0p6':'L1_DoubleEG10p5er1p22_dR_0p6',
 'L1_DoubleMu0_upt6_IP_Min1_upt4':'L1_DoubleMu0_upt6ip123_upt4',
-'L1_DoubleMu18er2p1_SQ':'L1_DoubleMu18er2p1'
+'L1_DoubleMu18er2p1_SQ':'L1_DoubleMu18er2p1',
+'L1_DoubleMu0_Upt15_Upt7': 'FALSE',
+'L1_DoubleMu3er2p0_SQ_OS_dR_Max1p4': 'FALSE',
+'L1_ETMHF90_SingleJet60er2p5_ETMHF90_DPHI_MIN2p094': 'FALSE',
+#'L1_MuShower_OneNominal': 'L1_ETMHF100 OR L1_ETMHF110 OR L1_ETM150 OR L1_ETMHF120 OR L1_ETMHF150',
+
 }
 
 for line in fileinput.input(text, inplace=True):
