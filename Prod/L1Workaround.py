7ae75838c44d685cecb27215b07306af2eec2e5d
Sanu Varghese <74061496+sanuvarghese@users.noreply.github.com>
2022-06-08 14:17:53 +0530

    Create L1Workaround.py

---
+++ Prod/L1Workaround.py
@@ -0,0 +1,34 @@
+import fileinput
+import os
+text = "hlt.py"
+
+match = {
+'L1_DoubleIsoTau26er2p1_Jet70_RmOvlp_dR0p5':'L1_SingleMuCosmics',
+'L1_Mu18er2p1_Tau26er2p1_Jet70':'L1_SingleMuCosmics',
+'L1_Mu18er2p1_Tau26er2p1_Jet55':'L1_SingleMuCosmics',
+
+}
+
+for line in fileinput.input(text, inplace=True):
+    line = line.rstrip()
+    if not line:
+        continue
+    for f_key, f_value in match.items():
+        if f_key in line:
+            line = line.replace(f_key, f_value)
+   
+    
+import re
+
+
+f = open("hlt.py","r")
+data = f.read() # string of all file content
+
+def replace_all(text, dic):
+    for i, j in dic.items():
+        text = re.sub(r"\b%s\b"%i, j, text) 
+        # r"\b%s\b"% enables replacing by whole word matches only
+    return text
+
+data = replace_all(data,match)
+print("Renaming Changes Undone") 
