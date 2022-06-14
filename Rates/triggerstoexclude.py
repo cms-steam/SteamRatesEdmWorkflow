ff4f698302d546a7a771563bf6eb899320621f26
Sanu Varghese <74061496+sanuvarghese@users.noreply.github.com>
2022-06-14 20:29:42 +0530

    Fix

--- Rates/triggerstoexclude.py
+++ Rates/triggerstoexclude.py
@@ -1,14 +1,15 @@
 triggerstoexcl=[
-'HLT_DoubleMu4_LowMass_Displaced_v',
-'HLT_DoubleMu4_3_LowMass_v',
-'HLT_L2Mu23NoVtx_2Cha_v',
-'HLT_Mu3_L1SingleMu5orSingleMu7_v',
-'HLT_L2Mu23NoVtx_2Cha_CosmicSeed_v',
-'HLT_EphemeralPhysics_v',
-'HLT_EphemeralZeroBias_v',
-'HLT_ExpressMuons_v',
-'HLT_PPSMaxTracksPerArm1_v',
-'HLT_OnlineMonitorGroup_v',
+'HLT_DoubleMu4_LowMass_Displaced_v1',
+'HLT_DoubleMu4_3_LowMass_v1',
+'HLT_L2Mu23NoVtx_2Cha_v1',
+'HLT_Mu3_L1SingleMu5orSingleMu7_v1',
+'HLT_L2Mu23NoVtx_2Cha_CosmicSeed_v1',
+'HLT_EphemeralPhysics_v1',
+'HLT_EphemeralZeroBias_v1',
+'HLT_ExpressMuons_v1',
+'HLT_PPSMaxTracksPerArm1_v1',
+'HLT_OnlineMonitorGroup_v1',
+'HLT_Mu0_L1DoubleMu_v1',
 ]
 
 
