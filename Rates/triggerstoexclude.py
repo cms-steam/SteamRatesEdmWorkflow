f3e1e80281e3252874fa83b66c86761cab64dc05
Sanu Varghese <74061496+sanuvarghese@users.noreply.github.com>
2022-06-14 17:28:04 +0530

    Add files via upload

---
+++ Rates/triggerstoexclude.py
@@ -0,0 +1,14 @@
+triggerstoexclude=[
+'HLT_DoubleMu4_LowMass_Displaced_v',
+'HLT_DoubleMu4_3_LowMass_v',
+'HLT_L2Mu23NoVtx_2Cha_v',
+'HLT_Mu3_L1SingleMu5orSingleMu7_v',
+'HLT_L2Mu23NoVtx_2Cha_CosmicSeed_v',
+'HLT_EphemeralPhysics_v',
+'HLT_EphemeralZeroBias_v',
+'HLT_ExpressMuons_v',
+'HLT_PPSMaxTracksPerArm1_v',
+'HLT_OnlineMonitorGroup_v',
+]
+
+
